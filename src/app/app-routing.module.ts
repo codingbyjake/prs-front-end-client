c81d79c144146caf4b5b7d9b5223335493681bf9
codingbyjake <110862321+codingbyjake@users.noreply.github.com>
2023-04-26 09:40:20 -0400

    Added User Class & Service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './core/home/home.component';
 import { AboutComponent } from './core/about/about.component';
+import { E404Component } from './core/e404/e404.component';
 
 const routes: Routes = [
- { path: "", redirectTo: "/core/home", pathMatch: "full"},
+ { path: "", redirectTo: "/home", pathMatch: "full"},
 
- { path: "core/home", component: HomeComponent },
- { path: "core/about", component: AboutComponent }
+ { path: "home", component: HomeComponent },
+ { path: "about", component: AboutComponent },
+
+ { path: "**", component: E404Component}
 
 ];
 
---
+++ src/app/user/user.class.ts
@@ -0,0 +1,12 @@
+export class User {
+    id: number = 0;
+    username: string = "";
+    password: string = "";
+    firstname: string = "";
+    lastname: string = "";
+    phone: string | null = null;
+    email: string | null = null;
+
+    isReviewer: boolean = false;
+    isAdmin: boolean = false;
+}
