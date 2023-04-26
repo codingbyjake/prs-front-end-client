2001a283cafbf33bdf8676659d9c151935716efd
codingbyjake <110862321+codingbyjake@users.noreply.github.com>
2023-04-26 13:46:29 -0400

    Added  User Create

--- src/app/user/user-create/user-create.component.ts
+++ src/app/user/user-create/user-create.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { User } from '../user.class';
+import { UserService } from '../user.service';
+import { SystemService } from 'src/app/core/system.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-create',
@@ -6,5 +10,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-create.component.css']
 })
 export class UserCreateComponent {
+  user: User = new User(); 
+  pageTitle = "Create New User";
+  
+  constructor(
+    private usrSvc: UserService,
+    private sysSvc: SystemService,
+    private route: ActivatedRoute,
+    private router: Router
+
+  ){}
+  
+  save(){
+    this.usrSvc.create(this.user).subscribe({
+      next: (res) => {
+        console.debug("User Created!!");
+        this.router.navigateByUrl("/user/list");
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    })
+
+  }
+
+  ngOnInit(): void{
+
+  }
 
 }
