3a102675f9e3cd179dd64de1d528bfae5664552b
codingbyjake <110862321+codingbyjake@users.noreply.github.com>
2023-05-02 14:35:01 -0400

    Fixed User selection for Create Request

--- src/app/request/request-create/request-create.component.ts
+++ src/app/request/request-create/request-create.component.ts
@@ -4,6 +4,7 @@ import { User } from 'src/app/user/user.class';
 import { RequestService } from '../request.service';
 import { UserService } from 'src/app/user/user.service';
 import { Router } from '@angular/router';
+import { SystemService } from 'src/app/core/system.service';
 
 @Component({
   selector: 'app-request-create',
@@ -14,15 +15,18 @@ export class RequestCreateComponent {
   request: Request = new Request();
   users: User[] = [];
   pageTitle: string = "Create New Request";
+  currentUserName: string = this.sysSvc.loggedInUser === null ? "No User, Please Login" : this.sysSvc.loggedInUser.username;
+  currentUserId: number = this.sysSvc.loggedInUser === null ? 0 : this.sysSvc.loggedInUser.id;
 
   constructor(
     private reqSvc: RequestService,
     private usrSvc: UserService,
-    private router: Router
+    private router: Router,
+    private sysSvc: SystemService
   ){}
 
   save(): void{
-    this.request.userId = Number(this.request.userId); // <<<<<<<<<<<<< Is this needed????
+    this.request.userId = this.currentUserId; 
     console.debug("Request at start of save()", this.request)
     this.reqSvc.create(this.request).subscribe({
       next: (res) => {
