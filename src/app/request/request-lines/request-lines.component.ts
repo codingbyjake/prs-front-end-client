aaeb8671b571eac9e9dd1d9f3b3751cf87e75315
codingbyjake <110862321+codingbyjake@users.noreply.github.com>
2023-05-02 11:33:56 -0400

    Added Requestline Delete

--- src/app/request/request-lines/request-lines.component.ts
+++ src/app/request/request-lines/request-lines.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RequestService } from '../request.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Request } from '../request.class';
+import { RequestlineService } from 'src/app/requestline/requestline.service';
+import { Requestline } from 'src/app/requestline/requestline.class';
 
 
 @Component({
@@ -15,13 +17,35 @@ export class RequestLinesComponent {
   // requestlines Requestline[] = [];
   pageTitle: string = "Request Lines";
   showVerifyRemove: boolean = false;
+  requestline!: Requestline;
+  requestlineIdToDelete!: number;
 
   constructor(
     private reqSvc: RequestService,
+    private rlineSvc: RequestlineService,
     private route: ActivatedRoute,
     private router: Router
 
   ){}
+
+  remove(requestline: Requestline) {
+    this.showVerifyRemove = !this.showVerifyRemove;
+    this.requestlineIdToDelete = requestline.id;
+  }
+
+  removeVerified(): void{
+    console.debug("requestlineIdToDelete before remove", this.requestlineIdToDelete);
+    this.rlineSvc.remove(this.requestlineIdToDelete).subscribe({
+      next: (res) => {
+        console.debug("Requestline Deleted!!");
+        this.showVerifyRemove = !this.showVerifyRemove;
+        this.refresh();
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    })
+  }
 
   review(): void{
     this.reqSvc.review(this.request).subscribe({
