b35b57de99e4ee9fe5d8995ff2ca82aaaac2a04f
codingbyjake <110862321+codingbyjake@users.noreply.github.com>
2023-05-02 15:28:04 -0400

    Added deliveryMode default in Request class

--- src/app/request/request.class.ts
+++ src/app/request/request.class.ts
@@ -6,7 +6,7 @@ export class Request{
     description: string = "";
     justification: string = "";
     rejectionReason: string = "";
-    deliveryMode: string = "";
+    deliveryMode: string = "Pickup";
     status: string = "NEW";
     total: number = 0;
 
